8737ac4e77f7446998935c2ee1b24eafe0e723b7
Tom Hipkin <tomhipkin@gmail.com>
2016-04-20 14:12:06 +0100

    (feature): getting distinct themes from r4d

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 
 gem 'rake'
 gem 'rest-client'
+gem 'sparql'
+gem 'rdf-rdfxml'
+gem 'pry'
 
 group :test do
   gem 'rspec'
---
+++ bin/get_themes.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'rest-client'
+require 'json'
+require 'sparql'
+require 'rdf/rdfxml'
+
+require 'pry'
+
+repository = RDF::Repository.load('http://r4d.dfid.gov.uk/RDF/SKOS/Themes.rdf')
+sparql = SPARQL::Client.new(repository)
+
+theme_query = <<-SPARQL
+  PREFIX skos:    <http://www.w3.org/2004/02/skos/core#>
+
+  SELECT DISTINCT ?theme ?prefLabel
+  WHERE {
+    ?theme skos:inScheme <http://r4d.dfid.gov.uk/rdf/skos/Themes> ;
+           skos:prefLabel ?prefLabel .
+  }
+SPARQL
+
+results = sparql.query(theme_query)
+results.map! do |solution|
+  theme_slug = solution['theme'].to_s.sub(%r{http://.*#}, '')
+
+  {
+    label: theme_slug,
+    value: solution['prefLabel'].to_s
+  }
+end
+
+puts result
