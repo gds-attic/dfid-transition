8ff29f4712ec2fe27f313d371c176b13a6478747
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-04-22 15:01:10 +0100

    Inline register_countries
    Wasn't revealing any intent that the class method
    isn't telling us

--- lib/dfid-transition/patch/countries.rb
+++ lib/dfid-transition/patch/countries.rb
@@ -8,15 +8,11 @@ module DfidTransition
     class Countries < Base
 
       def mutate_schema
-        country_facet['allowed_values'] = transform_to_label_value(register_countries)
+        country_facet['allowed_values'] = transform_to_label_value(
+          DfidTransition::GovUkCountryRegister.countries)
       end
 
     private
-
-      def register_countries
-        DfidTransition::GovUkCountryRegister.countries
-      end
-
       def transform_to_label_value(query_results)
         query_results.map do |result|
           {
