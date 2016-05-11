f588024d1e232b53a8e995ae87cab99e8b88bcf7
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-05-11 11:52:40 +0100

    Rubocop module space offenses

--- lib/dfid-transition/transform/html.rb
+++ lib/dfid-transition/transform/html.rb
@@ -3,7 +3,6 @@ require 'cgi'
 module DfidTransition
   module Transform
     module Html
-
       def self.unescape_three_times(string_input)
         CGI.unescape_html(
           CGI.unescape_html(
