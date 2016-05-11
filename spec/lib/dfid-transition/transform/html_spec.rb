a37bb921d29e46d503e0a47d388ae8553090c8a0
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-05-11 07:57:10 +0100

    Spec Html.unescape_three_times in context/let style
    * We only need say what we're testing once, in the subject
    * The rest of the time we can describe context in terms of how the
      inputs are varying and what behaviour we expect
    * it { is_expected... } implicitly refers to `unescaped_html`
    * In the one example (it 'does not care') where we need to say why
      a thing might fail, we can refer to the subject explicitly via the
      name `unescaped_html`
    * Removing the hash/each_pair assertions in favour of this style
      means if a test fails, RSpec will tell us which one by line
    * Removed some redundant tests

--- spec/lib/dfid-transition/transform/html_spec.rb
+++ spec/lib/dfid-transition/transform/html_spec.rb
@@ -7,54 +7,31 @@ module DfidTransition::Transform
       DfidTransition::Transform::Html.unescape_three_times(string_input)
     end
 
-    context 'when given single html escaped HTML' do
+    context 'HTML is single-escaped' do
       let(:string_input) { '&lt;p&gt;hello&lt;/p&gt;' }
-
-      it "returns an unescaped string" do
-        expect(unescaped_html).to eql('<p>hello</p>')
-      end
+      it { is_expected.to eql('<p>hello</p>') }
     end
 
-    context 'it has double-escaped HTML' do
-      let(:double_escaped_html_examples) do
-        {
-          '&amp;#8216;Successful&amp;#8217; Development Models' =>
-            '‘Successful’ Development Models',
-          '&amp;lt;br/&amp;gt;&amp;lt;br/&amp;gt; This policy brief explores' =>
-            '<br/><br/> This policy brief explores',
-          'domestic violence.&amp;lt;/p&amp;gt;' =>
-            'domestic violence.</p>',
-          'various countries.&amp;lt;/p&amp;gt; &amp;lt;p&amp;gt;Given that both Mexico' =>
-            'various countries.</p> <p>Given that both Mexico'
-        }
+    context 'HTML is double-escaped' do
+      context 'There are special character encodes' do
+        let(:string_input) { '&amp;#8216;Successful&amp;#8217; Development Models' }
+        it { is_expected.to eql('‘Successful’ Development Models') }
       end
-
-      it 'returns an unescaped string' do
-        double_escaped_html_examples.each_pair do |escaped_string, desired_string|
-          expect(
-            DfidTransition::Transform::Html.unescape_three_times(escaped_string)
-          ).to eql(desired_string)
+      context 'There are double-escaped tags' do
+        let(:string_input) { '&amp;lt;br/&amp;gt;&amp;lt;br/&amp;gt; This policy brief explores' }
+        it { is_expected.to eql('<br/><br/> This policy brief explores') }
+      end
+      context 'there are unmatched tags' do
+        let(:string_input) { 'domestic violence.&amp;lt;/p&amp;gt;' }
+        it 'does not care' do
+          expect(unescaped_html).to eql('domestic violence.</p>')
         end
       end
     end
 
-    context 'is has triple-escaped HTML' do
-      let(:tripled_escaped_html_examples) do
-        {
-          'spread of India&amp;amp;#8217;s HIV epidemic' =>
-            'spread of India’s HIV epidemic',
-          'IPs&amp;amp;#8217; perceptions' =>
-            'IPs’ perceptions',
-        }
-      end
-
-      it 'returns an unescaped string' do
-        tripled_escaped_html_examples.each_pair do |escaped_string, desired_string|
-          expect(
-            DfidTransition::Transform::Html.unescape_three_times(escaped_string)
-          ).to eql(desired_string)
-        end
-      end
+    context 'HTML is triple-escaped' do
+      let(:string_input) { 'spread of India&amp;amp;#8217;s HIV epidemic' }
+      it { is_expected.to eql('spread of India’s HIV epidemic') }
     end
   end
 
