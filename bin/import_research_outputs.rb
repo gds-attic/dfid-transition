2ae9771eb477705c355e12576ac1141d9a3c95a3
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-05-12 13:22:13 +0100

    Move SearchPresenter to tested class
    * No longer needs publishing_api (has fixed organisation_slugs)
    * Has a spec.

--- bin/import_research_outputs.rb
+++ bin/import_research_outputs.rb
@@ -14,43 +14,9 @@ require 'gds_api/rummager'
 require 'active_support/core_ext/module/delegation'
 require 'active_support/core_ext/string'
 require 'dfid-transition/transform/document'
+require 'govuk/presenters/search'
 
 ENDPOINT = 'http://linked-development.org/sparql'
-
-class SearchPresenter
-  delegate :title, to: :document
-
-  def initialize(document, publishing_api)
-    @document = document
-    @publishing_api = publishing_api
-  end
-
-  def to_json
-    {
-      title: document.title,
-      description: document.abstract,
-      link: document.base_path,
-      indexable_content: indexable_content,
-      organisations: organisation_slugs,
-      public_timestamp: document.public_updated_at.to_datetime.rfc3339,
-    }.merge(document.format_specific_metadata).reject { |_k, v| v.blank? }
-  end
-
-  def indexable_content
-    document.body.last[:content]
-  end
-
-  def organisation_slugs
-    response = publishing_api.get_content_items(document_type: "organisation", fields: [:content_id, :base_path])
-
-    orgs = response.results.select { |o| document.organisations.include?(o["content_id"]) }
-    orgs.map { |o| o["base_path"].gsub("/government/organisations/", "") }.map { |o| o.gsub("/courts-tribunals/", "") }
-  end
-
-  private
-
-  attr_reader :document, :publishing_api
-end
 
 publishing_api = GdsApi::PublishingApiV2.new(
   Plek.new.find('publishing-api'),
@@ -83,7 +49,7 @@ WHERE {
            geo:codeISO2 ?codeISO2 .
 } GROUP BY ?output ?date ?abstract
 ORDER BY DESC(?date)
-LIMIT 100
+LIMIT 10
 SPARQL
 
 client = SPARQL::Client.new(ENDPOINT, method: :get)
@@ -107,6 +73,6 @@ output_solutions.each do |output_solution|
   rummager.add_document(
     'dfid_research_output',
     doc.base_path,
-    SearchPresenter.new(doc, publishing_api).to_json,
+    Govuk::Presenters::Search.new(doc).to_json,
   )
 end
