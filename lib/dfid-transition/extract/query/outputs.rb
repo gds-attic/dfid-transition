53fe735babffcca88dfe89d967692edb1e36c21c
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-05-19 09:16:44 +0100

    Add authors to research output body
    * Comes with its own top-level header
    * Renders as a list
    * In the SPARQL query:
      * Start to use property paths in the query apropos of the fact that
       creators use bnodes for uniqueness (authors that are distinct but
       have no URL). As a result, only countries have an ISO2 code, so we
       can drop the self_governing type check for an intermediate bound
       ?country variable, which helps speed things up
      * make sure that OPTIONAL things are really OPTIONAL (previously we
       were only retrieving outputs with a self_governing country)
      * Pack authors as a pipe-delimited list

--- lib/dfid-transition/extract/query/outputs.rb
+++ lib/dfid-transition/extract/query/outputs.rb
@@ -10,21 +10,24 @@ module DfidTransition
           PREFIX dcterms: <http://purl.org/dc/terms/>
           PREFIX ont:     <http://purl.org/ontology/bibo/>
           PREFIX geo:     <http://www.fao.org/countryprofiles/geoinfo/geopolitical/resource/>
+          PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
 
           SELECT DISTINCT ?output ?date ?abstract
+                          (GROUP_CONCAT(DISTINCT(?creator); separator = '|') AS ?creators)
                           (GROUP_CONCAT(DISTINCT(?titleSource)) AS ?title)
                           (GROUP_CONCAT(DISTINCT(?codeISO2)) AS ?countryCodes)
           WHERE {
             ?output a ont:Article ;
                     dcterms:title ?titleSource ;
                     dcterms:abstract ?abstract ;
-                    dcterms:date ?date ;
-                    dcterms:coverage ?country .
-            ?country a geo:self_governing ;
-                     geo:codeISO2 ?codeISO2 .
+                    dcterms:date ?date .
+
+            OPTIONAL { ?output dcterms:coverage/geo:codeISO2 ?codeISO2 }
+            OPTIONAL { ?output dcterms:creator/foaf:name     ?creator }
+
           } GROUP BY ?output ?date ?abstract
           ORDER BY DESC(?date)
-          LIMIT 10
+          LIMIT 20
         SPARQL
 
         def endpoint
