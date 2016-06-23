3d40eab788b405a78966ece5493e148eccd6a415
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-06-23 17:36:26 +0100

    Add Attachment#filename
    Derived from #original_url

--- lib/dfid-transition/transform/attachment.rb
+++ lib/dfid-transition/transform/attachment.rb
@@ -3,13 +3,19 @@ require 'uri'
 module DfidTransition
   module Transform
     class Attachment
+      attr_reader :original_url
+
       def initialize(original_url)
         @original_url = URI(original_url)
         raise ArgumentError, "expected a URL, got #{original_url}" unless @original_url.is_a?(URI::HTTP)
       end
 
       def hosted_at_r4d?
-        @original_url.host == 'r4d.dfid.gov.uk'
+        original_url.host == 'r4d.dfid.gov.uk'
+      end
+
+      def filename
+        File.basename(original_url.path).sub('/', '')
       end
     end
   end
--- spec/lib/dfid-transition/transform/attachment_spec.rb
+++ spec/lib/dfid-transition/transform/attachment_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+ require 'spec_helper'
 require 'dfid-transition/transform/attachment'
 
 module DfidTransition::Transform
@@ -25,6 +25,27 @@ module DfidTransition::Transform
       context 'given a URL that is onsite' do
         let(:original_url) { 'http://r4d.dfid.gov.uk/some/file.pdf' }
         it { is_expected.to be true }
+      end
+    end
+
+    describe '#filename' do
+      context 'a filename with an extension' do
+        let(:original_url) { 'http://example.com/some/path/title_of_the_file.pdf' }
+        it 'uses the filename' do
+          expect(attachment.filename).to eql('title_of_the_file.pdf')
+        end
+      end
+
+      context 'a path but no extension' do
+        let(:original_url) { 'http://example.com/some/path/' }
+        it 'uses the last part of the path' do
+          expect(attachment.filename).to eql('path')
+        end
+      end
+
+      context 'no path at all' do
+        let(:original_url) { 'http://example.com/' }
+        example { expect(attachment.filename).to be_empty }
       end
     end
   end
