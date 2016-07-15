2a6860f97841b525f46f123acb42508da21bd578
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-07-15 15:26:40 +0100

    Add an AttachmentIndex, which can:
    Store an asset manager response as returned from gds-api-adapters.
    Get it back later.

--- lib/dfid-transition/services.rb
+++ lib/dfid-transition/services.rb
@@ -1,6 +1,7 @@
 require 'gds_api/publishing_api_v2'
 require 'gds_api/asset_manager'
 require 'gds_api/rummager'
+require 'dfid-transition/services/attachment_index'
 
 module DfidTransition
   module Services
@@ -20,6 +21,15 @@ module DfidTransition
 
     def self.rummager
       @rummager ||= GdsApi::Rummager.new(Plek.new.find('search'))
+    end
+
+    def self.attachment_index
+      @attachment_index ||= AttachmentIndex.new(redis)
+    end
+
+    def self.redis
+      require_relative '../../config/initializers/redis'
+      @redis ||= _namespaced_redis
     end
   end
 end
