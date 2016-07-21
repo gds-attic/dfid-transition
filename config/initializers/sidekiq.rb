091566dbd4d777cc01c974d3d786912207e9ed12
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-07-21 12:52:48 +0100

    Make sure we Sidekiq.configure_server
    Failure to do so will cause .configure_client to fail to connect with
     default localhost settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,6 +3,10 @@ require_relative './redis'
 require 'sidekiq'
 require 'sidekiq/api'
 
+Sidekiq.configure_server do |config|
+  config.redis = _redis_config
+end
+
 Sidekiq.configure_client do |config|
   config.redis = _redis_config
 end
